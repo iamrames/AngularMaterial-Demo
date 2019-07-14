4010fa7d2ff91337ee95dfeb82b70e2e59a8f5ac
iamrames <addictedtomig@gmail.com>
2019-07-14 10:56:23 +0545

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+enum sex {male, female}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-demo';
+  isChecked = true;
+  colors = [
+    { id: 0, name: 'Select Colors'},
+    { id: 1, name: 'Red'},
+    { id: 2, name: 'Blue'},
+    { id: 3, name: 'Green'},
+  ];
+
+  onChange($event) {
+    console.log($event);
+  }
+
+  onChangeRadio($event) {
+    console.log($event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule, MatRadioModule, MatSelectModule, MatInputModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
